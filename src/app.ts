8375b9d892042093850adc405c91f97f3f28a1a8
Wooju <dnwn810@naver.com>
2026-07-01 17:08:46 +0900

    fix:버셀 도메인 CORS허용 추가

--- src/app.ts
+++ src/app.ts
@@ -22,7 +22,7 @@ app.use(
   cors({
     origin:
       process.env.NODE_ENV === "production"
-        ? ["https://5nack.site"]
+        ? ["https://5nack.site", "https://6-snack-fe.vercel.app"]
         : ["http://localhost:3000", "http://localhost:3001", "http://localhost:5173", "http://localhost:8080"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
